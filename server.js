95c144013945d0b103ca130f51bd323a1f1cd592
chavesguido <chavesguido@hotmail.com>
2018-07-30 16:18:37 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require ('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require ('./controllers/image.js');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'm2laks9e1',
+		database: 'smart-brain'
+	}
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, () => {
+	console.log('server levantado en puerto 3000');
+})
+
+app.get('/', (req, res) => { res.json(database.users) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(get, res, db)})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
